032cfabdb0065f7782c2d0946c38253eaaf7c1a8
士允 王 <terryswang0729@gmail.com>
2022-11-12 12:49:45 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IEnumerable<int> range = Enumerable.Range(1, 10);
+			foreach(int i in range)
+			{
+				Console.WriteLine(i);
+			}
+	}   }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IEnumerable<int> range = Enumerable.Range(10, 6);
+			foreach (var item in range)
+			{
+				Console.WriteLine(item);
+			}
+		}
+	}
+}
